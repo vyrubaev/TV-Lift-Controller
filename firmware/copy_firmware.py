edc9ba853282dc708fab65dc5b0e8b5b2dac629a
Vyrubaev <58784103+vyrubaev@users.noreply.github.com>
2026-09-03 04:15:28 +0400

    test ota

--- firmware/copy_firmware.py
+++ firmware/copy_firmware.py
@@ -1,26 +1,37 @@
-import os
-import shutil
 import json
+import os
 import re
+import shutil
 from datetime import datetime
+from typing import Any
+from importlib import import_module
+
+# SCons доступен только в окружении PlatformIO
+Import = import_module("SCons.Script").Import
+
+env: Any
+Import("env")
 
 print("\n>>> [ЧЕКЕР] ПИТОН-СКРИПТ ИНИЦИАЛИЗИРОВАН ПЛАТФОРМИО! <<<\n")
 
-# 1. Пути к файлам
-project_dir = os.getcwd()
-header_file = os.path.join(project_dir, "src", "OTA", "OtaUpdater.h")
+# 1. Точные пути через окружение PlatformIO (берем DeviceConfig.h )
+project_dir = env.subst("$PROJECT_DIR")
+header_file = os.path.join(project_dir, "src", "Config/DeviceConfig.h") 
 target_dir = os.path.abspath(os.path.join(project_dir, "../backend/public/firmware"))
 json_file = os.path.join(target_dir, "version.json")
 bin_source = os.path.join(project_dir, ".pio", "build", "esp32dev", "firmware.bin")
 bin_target = os.path.join(target_dir, "firmware.bin")
 
-# 2. Ищем версию в OtaUpdater.h
+# 2. Ищем версию в DeviceConfig.h
 version = "1.0.0"
 if os.path.exists(header_file):
     with open(header_file, "r", encoding="utf-8") as f:
-        match = re.search(r'#define\s+DeviceConfig::VERSION\s+"([^"]+)"', f.read())
+        content = f.read()
+        match = re.search(r'VERSION\s*=\s*"([^"]+)"', content)
         if match:
             version = match.group(1)
+else:
+    print(f"[OTA ОШИБКА] Файл не найден по пути: {header_file}")
 
 print(f"[OTA] Прочитана версия из файла: {version}")
 
@@ -43,5 +54,7 @@ print(f"[OTA] Записан JSON: {json_file}")
 if os.path.exists(bin_source):
     shutil.copy(bin_source, bin_target)
     print(f"[OTA] Скопирован бинарник -> {bin_target}")
+else:
+    print(f"[OTA ОШИБКА] Бинарник не найден по пути: {bin_source}")
 
 print(">>> [ЧЕКЕР] СКРИПТ УСПЕШНО ЗАВЕРШИЛ РАБОТУ! <<<\n")
